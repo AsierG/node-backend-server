fc08ffdb22f3734d116112aeebd338a8731b9aad
Asier Garcia <asier.garcia.garcia@everis.com>
2019-01-30 13:36:38 +0100

    add general search

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var loginRoutes = require('./routes/login');
 var usuarioRoutes = require('./routes/usuario');
 var hospitalRoutes = require('./routes/hospital');
 var medicoRoutes = require('./routes/medico');
+var busquedaRoutes = require('./routes/busqueda');
 
 //Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -33,6 +34,8 @@ app.use('/usuario', usuarioRoutes);
 app.use('/hospital', hospitalRoutes);
 app.use('/medico', medicoRoutes);
 app.use('/login', loginRoutes);
+app.use('/busqueda', busquedaRoutes);
+
 app.use('/', appRoutes);
 
 //Escuchar peticiones
