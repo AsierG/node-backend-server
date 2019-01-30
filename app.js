05d99c8fd3912f3e4c053071a108daccca2d8576
Asier Garcia <asier.garcia.garcia@everis.com>
2019-01-30 17:05:49 +0100

    upload images

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var usuarioRoutes = require('./routes/usuario');
 var hospitalRoutes = require('./routes/hospital');
 var medicoRoutes = require('./routes/medico');
 var busquedaRoutes = require('./routes/busqueda');
+var uploadRoutes = require('./routes/upload');
 
 //Conexión a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -35,6 +36,7 @@ app.use('/hospital', hospitalRoutes);
 app.use('/medico', medicoRoutes);
 app.use('/login', loginRoutes);
 app.use('/busqueda', busquedaRoutes);
+app.use('/upload', uploadRoutes);
 
 app.use('/', appRoutes);
 
--- routes/busqueda.js
+++ routes/busqueda.js
@@ -7,7 +7,43 @@ var Hospital = require('../models/hospital');
 var Medico = require('../models/medico');
 var Usuario = require('../models/usuario');
 
-//Rutas
+// Busqueda por coleccion
+app.get('/coleccion/:tabla/:busqueda', (req, res) => {
+
+    var busqueda = req.params.busqueda;
+    var tabla = req.params.tabla;
+    var regex = new RegExp(busqueda, 'i');
+
+    var promesa;
+
+    switch (tabla) {
+        case 'usuarios':
+            promesa = buscarUsuarios(busqueda, regex);
+            break;
+        case 'medicos':
+            promesa = buscarMedicos(busqueda, regex);
+            break;
+        case 'hospitales':
+            promesa = buscarHospitales(busqueda, regex);
+            break;
+        default:
+            res.status(400).json({
+                ok: false,
+                mensaje: 'Los tipos de busqueda sólo son: usuarios, medicos y hospitales',
+                error: { message: 'Tipo de tabla/colección no válido' }
+            });
+    }
+
+    promesa.then(data => {
+        res.status(200).json({
+            ok: false,
+            [tabla]: data
+        });
+    });
+
+});
+
+// Busqueda general
 app.get('/todo/:busqueda', (req, res, next) => {
 
     var busqueda = req.params.busqueda;
@@ -19,20 +55,15 @@ app.get('/todo/:busqueda', (req, res, next) => {
             buscarUsuarios(busqueda, regex)
         ])
         .then(respuestas => {
-
             res.status(200).json({
                 ok: true,
                 hospitales: respuestas[0],
                 medicos: respuestas[1],
                 usuarios: respuestas[2]
             });
-
         });
-
-
-
-
 });
+
 
 function buscarHospitales(busqueda, regex) {
     return new Promise((resolve, reject) => {
@@ -75,7 +106,6 @@ function buscarUsuarios(busqueda, regex) {
                     resolve(usuarios);
                 }
             });
-
     });
 }
 
